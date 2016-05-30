87b7131ac36054cc9dba42a2e6182f8b8a224b05
Gabriele Petronella <gabriele@buildo.io>
2016-05-30 20:31:51 +0200

    Add "basic" combinators and tests

--- tests/lib/rules/no-unnamed-types.js
+++ tests/lib/rules/no-unnamed-types.js
@@ -43,9 +43,6 @@ ruleTester.run('no-unnamed-types', rule, {
       code: 't.union([A, B], \'MyType\')'
     },
     {
-      code: 't.dict(t.Number, t.String, \'MyType\')'
-    },
-    {
       code: 't.intersection([A, B], \'MyType\')'
     },
     {
@@ -60,10 +57,50 @@ ruleTester.run('no-unnamed-types', rule, {
   ],
 
   invalid: [
-    // {
-    //   code: 't.struct({})',
-    //   errors: [ { message: 'use of loose tcomb structs is forbidden. Add option \'{ strict: true }\'' } ],
-    // }
+    {
+      code: 't.refinement(t.Number, function () { return true })',
+      errors: [ { message: 'refinement must have a name' } ],
+    },
+    {
+      code: 't.enums({ IT: \'IT\' })',
+      errors: [ { message: 'enums must have a name' } ],
+    },
+    {
+      code: 't.maybe(t.String)',
+      errors: [ { message: 'maybe must have a name' } ],
+    },
+    {
+      code: 't.struct({})',
+      errors: [ { message: 'struct must have a name' } ],
+    },
+    {
+      code: 't.tuple([t.Number, t.Number])',
+      errors: [ { message: 'tuple must have a name' } ],
+    },
+    {
+      code: 't.list(t.Number)',
+      errors: [ { message: 'list must have a name' } ],
+    },
+    {
+      code: 't.dict(t.Number, t.String)',
+      errors: [ { message: 'dict must have a name' } ],
+    },
+    {
+      code: 't.union([A, B])',
+      errors: [ { message: 'union must have a name' } ],
+    },
+    {
+      code: 't.intersection([A, B])',
+      errors: [ { message: 'intersection must have a name' } ],
+    },
+    {
+      code: 't.interface({})',
+      errors: [ { message: 'interface must have a name' } ],
+    },
+    {
+      code: 't.func([A], B )',
+      errors: [ { message: 'func must have a name' } ],
+    },
   ]
 });
 
