1c2362ee3c774e0759df5937a8b3d6c7b64ca821
Gabriele Petronella <gabriele@buildo.io>
2016-05-30 21:10:16 +0200

    Add options

--- tests/lib/rules/no-unnamed-types.js
+++ tests/lib/rules/no-unnamed-types.js
@@ -60,58 +60,72 @@ ruleTester.run('no-unnamed-types', rule, {
     {
       code: 't.refinement(t.Number, function () { return true })',
       errors: [ { message: 'refinement must have a name' } ],
+      options: [{ enabled: { refinement: true } }]
     },
     {
       code: 't.enums({ IT: \'IT\' })',
       errors: [ { message: 'enums must have a name' } ],
+      options: [{ enabled: { enums: true } }]
     },
     {
       code: 't.enums.of({ IT: \'IT\' })',
       errors: [ { message: 'enums must have a name' } ],
+      options: [{ enabled: { enums: true } }]
     },
     {
       code: 't.maybe(t.String)',
       errors: [ { message: 'maybe must have a name' } ],
+      options: [{ enabled: { maybe: true } }]
     },
     {
       code: 't.struct({})',
       errors: [ { message: 'struct must have a name' } ],
+      options: [{ enabled: { struct: true } }]
     },
     {
       code: 't.struct.extend([Type, {}])',
       errors: [ { message: 'struct must have a name' } ],
+      options: [{ enabled: { struct: true } }]
     },
     {
       code: 't.tuple([t.Number, t.Number])',
       errors: [ { message: 'tuple must have a name' } ],
+      options: [{ enabled: { tuple: true } }]
     },
     {
       code: 't.list(t.Number)',
       errors: [ { message: 'list must have a name' } ],
+      options: [{ enabled: { list: true } }]
     },
     {
       code: 't.dict(t.Number, t.String)',
       errors: [ { message: 'dict must have a name' } ],
+      options: [{ enabled: { dict: true } }]
     },
     {
       code: 't.union([A, B])',
       errors: [ { message: 'union must have a name' } ],
+      options: [{ enabled: { union: true } }]
     },
     {
       code: 't.intersection([A, B])',
       errors: [ { message: 'intersection must have a name' } ],
+      options: [{ enabled: { intersection: true } }]
     },
     {
       code: 't.interface({})',
       errors: [ { message: 'interface must have a name' } ],
+      options: [{ enabled: { interface: true } }]
     },
     {
       code: 't.interface.extend([Type, {}])',
       errors: [ { message: 'interface must have a name' } ],
+      options: [{ enabled: { interface: true } }]
     },
     {
       code: 't.func([A], B )',
       errors: [ { message: 'func must have a name' } ],
+      options: [{ enabled: { func: true } }]
     },
   ]
 });
