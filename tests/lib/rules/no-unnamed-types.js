6b46469a85eae3a3e5e3b9614646be759451e1b0
Gabriele Petronella <gabriele@buildo.io>
2016-06-02 13:57:05 +0200

    Make no-unnamed-types use tcombNamspace util

--- tests/lib/rules/no-unnamed-types.js
+++ tests/lib/rules/no-unnamed-types.js
@@ -7,131 +7,227 @@ var ruleTester = new RuleTester();
 ruleTester.run('no-unnamed-types', rule, {
   valid: [
     {
-      code: 'foo(\'bar\')',
+      code: [
+        'var t = require("tcomb");',
+        'foo(\'bar\')'
+      ].join('\n')
     },
     {
-      code: 't.refinement(t.Number, function () { return true }, \'MyType\')',
+      code: [
+        'var t = require("tcomb");',
+        't.refinement(t.Number, function () { return true }, \'MyType\')'
+      ].join('\n')
     },
     {
-      code: 't.enums({ IT: \'IT\' }, \'MyType\')',
+      code: [
+        'var t = require("tcomb");',
+        't.enums({ IT: \'IT\' }, \'MyType\')'
+      ].join('\n')
     },
     {
-      code: 't.enums.of([\'IT\'], \'MyType\')',
+      code: [
+        'var t = require("tcomb");',
+        't.enums.of([\'IT\'], \'MyType\')'
+      ].join('\n')
     },
     {
-      code: 't.maybe(t.String, \'MyType\')',
+      code: [
+        'var t = require("tcomb");',
+        't.maybe(t.String, \'MyType\')'
+      ].join('\n')
     },
     {
-      code: 't.struct({}, \'MyType\')',
+      code: [
+        'var t = require("tcomb");',
+        't.struct({}, \'MyType\')'
+      ].join('\n')
     },
     {
-      code: 't.struct({}, { name: \'MyType\' })',
+      code: [
+        'var t = require("tcomb");',
+        't.struct({}, { name: \'MyType\' })'
+      ].join('\n')
     },
     {
-      code: 'Type.extend({}, \'MyType\')'
+      code: [
+        'var t = require("tcomb");',
+        'Type.extend({}, \'MyType\')'
+      ].join('\n')
     },
     {
-      code: 't.struct.extend([Type, {}], \'MyType\')'
+      code: [
+        'var t = require("tcomb");',
+        't.struct.extend([Type, {}], \'MyType\')'
+      ].join('\n')
     },
     {
-      code: 't.tuple([t.Number, t.Number], \'MyType\')'
+      code: [
+        'var t = require("tcomb");',
+        't.tuple([t.Number, t.Number], \'MyType\')'
+      ].join('\n')
     },
     {
-      code: 't.list(t.Number, \'MyType\')'
+      code: [
+        'var t = require("tcomb");',
+        't.list(t.Number, \'MyType\')'
+      ].join('\n')
     },
     {
-      code: 't.dict(t.Number, t.String, \'MyType\')'
+      code: [
+        'var t = require("tcomb");',
+        't.dict(t.Number, t.String, \'MyType\')'
+      ].join('\n')
     },
     {
-      code: 't.union([A, B], \'MyType\')'
+      code: [
+        'var t = require("tcomb");',
+        't.union([A, B], \'MyType\')'
+      ].join('\n')
     },
     {
-      code: 't.intersection([A, B], \'MyType\')'
+      code: [
+        'var t = require("tcomb");',
+        't.intersection([A, B], \'MyType\')'
+      ].join('\n')
     },
     {
-      code: 't.interface({}, \'MyType\')'
+      code: [
+        'var t = require("tcomb");',
+        't.interface({}, \'MyType\')'
+      ].join('\n')
     },
     {
-      code: 't.interface.extend([Type, {}], \'MyType\')'
+      code: [
+        'var t = require("tcomb");',
+        't.interface.extend([Type, {}], \'MyType\')'
+      ].join('\n')
     },
     {
-      code: 't.func([A], B, \'MyType\')'
+      code: [
+        'var t = require("tcomb");',
+        't.func([A], B, \'MyType\')'
+      ].join('\n')
     }
   ],
 
   invalid: [
     {
-      code: 't.refinement(t.Number, function () { return true })',
+      code: [
+        'var t = require("tcomb");',
+        't.refinement(t.Number, function () { return true })'
+      ].join('\n'),
       errors: [ { message: 'refinement must have a name' } ],
       options: [{ enabled: { refinement: true } }]
     },
     {
-      code: 't.enums({ IT: \'IT\' })',
+      code: [
+        'var t = require("tcomb");',
+        't.enums({ IT: \'IT\' })'
+      ].join('\n'),
       errors: [ { message: 'enums must have a name' } ],
       options: [{ enabled: { enums: true } }]
     },
     {
-      code: 't.enums.of({ IT: \'IT\' })',
+      code: [
+        'var t = require("tcomb");',
+        't.enums.of({ IT: \'IT\' })'
+      ].join('\n'),
       errors: [ { message: 'enums must have a name' } ],
       options: [{ enabled: { enums: true } }]
     },
     {
-      code: 't.maybe(t.String)',
+      code: [
+        'var t = require("tcomb");',
+        't.maybe(t.String)'
+      ].join('\n'),
       errors: [ { message: 'maybe must have a name' } ],
       options: [{ enabled: { maybe: true } }]
     },
     {
-      code: 't.struct({})',
+      code: [
+        'var t = require("tcomb");',
+        't.struct({})'
+      ].join('\n'),
       errors: [ { message: 'struct must have a name' } ],
       options: [{ enabled: { struct: true } }]
     },
     {
-      code: 't.struct({}, { strict: true })',
+      code: [
+        'var t = require("tcomb");',
+        't.struct({}, { strict: true })'
+      ].join('\n'),
       errors: [ { message: 'struct must have a name' } ],
       options: [{ enabled: { struct: true } }]
     },
     {
-      code: 't.struct.extend([Type, {}])',
+      code: [
+        'var t = require("tcomb");',
+        't.struct.extend([Type, {}])'
+      ].join('\n'),
       errors: [ { message: 'struct must have a name' } ],
       options: [{ enabled: { struct: true } }]
     },
     {
-      code: 't.tuple([t.Number, t.Number])',
+      code: [
+        'var t = require("tcomb");',
+        't.tuple([t.Number, t.Number])'
+      ].join('\n'),
       errors: [ { message: 'tuple must have a name' } ],
       options: [{ enabled: { tuple: true } }]
     },
     {
-      code: 't.list(t.Number)',
+      code: [
+        'var t = require("tcomb");',
+        't.list(t.Number)'
+      ].join('\n'),
       errors: [ { message: 'list must have a name' } ],
       options: [{ enabled: { list: true } }]
     },
     {
-      code: 't.dict(t.Number, t.String)',
+      code: [
+        'var t = require("tcomb");',
+        't.dict(t.Number, t.String)'
+      ].join('\n'),
       errors: [ { message: 'dict must have a name' } ],
       options: [{ enabled: { dict: true } }]
     },
     {
-      code: 't.union([A, B])',
+      code: [
+        'var t = require("tcomb");',
+        't.union([A, B])'
+      ].join('\n'),
       errors: [ { message: 'union must have a name' } ],
       options: [{ enabled: { union: true } }]
     },
     {
-      code: 't.intersection([A, B])',
+      code: [
+        'var t = require("tcomb");',
+        't.intersection([A, B])'
+      ].join('\n'),
       errors: [ { message: 'intersection must have a name' } ],
       options: [{ enabled: { intersection: true } }]
     },
     {
-      code: 't.interface({})',
+      code: [
+        'var t = require("tcomb");',
+        't.interface({})'
+      ].join('\n'),
       errors: [ { message: 'interface must have a name' } ],
       options: [{ enabled: { interface: true } }]
     },
     {
-      code: 't.interface.extend([Type, {}])',
+      code: [
+        'var t = require("tcomb");',
+        't.interface.extend([Type, {}])'
+      ].join('\n'),
       errors: [ { message: 'interface must have a name' } ],
       options: [{ enabled: { interface: true } }]
     },
     {
-      code: 't.func([A], B )',
+      code: [
+        'var t = require("tcomb");',
+        't.func([A], B )'
+      ].join('\n'),
       errors: [ { message: 'func must have a name' } ],
       options: [{ enabled: { func: true } }]
     }
